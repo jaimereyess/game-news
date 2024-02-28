1aab6d5690ee1b07b145edec0dac87502eaf9ba1
Jaime Reyes <jaimereyesrdgz@gmail.com>
2024-02-28 23:48:30 +0000

    vercel produccion

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://jaimereyess.github.io/game-news/', 
-  build: {
-    outDir: 'dist'
-
-  }
-})
+});
